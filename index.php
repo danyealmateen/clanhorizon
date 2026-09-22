046fee83518069c50d82387b9710e45f289da342
danyealmateen <danyeal.mateen@hotmail.com>
2026-09-22 21:55:20 +0200

    more tests with css

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "Tjena test - och så testar jag igen" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clan Horizon</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="brand-header">
+        <h1>Clan Horizon</h1>
+    </header>
+</body>
+</html>
